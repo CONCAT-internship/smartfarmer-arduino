2195fda5a5a387a7cce04387d56c225d57406810
unknown <zczczcc123@naver.com>
2020-07-27 16:06:31 +0900

    post까지 완료한 상태 get은 확인 후 업로드 예정

--- src/hardware/smart_farm_sensor/smart_farm_sensor.ino
+++ src/hardware/smart_farm_sensor/smart_farm_sensor.ino
@@ -1,37 +1,147 @@
 #include <EtherCard.h>
 #include <ArduinoJson.h>
 #include <DHT11.h>
-#include <OneWire.h> 
+#include <OneWire.h>
+#include <ArduinoUniqueID.h>
+#include <EEPROM.h>
+#include "GravityTDS.h"
+#include <DallasTemperature.h>
 
-#define board_rate 115200
-#define liquid_temperature_pin 2
-#define liquid_level_pin 5
+#define board_rate 11520
+#define liquid_temperature_pin 20
+#define humidity_temperature_pin 4
+#define pH_pin A14
+#define ec_pin A15
+#define liquid_level_pin 21
+#define light_pin A0
+#define fan_pin 11
 
-DHT11 dht11(4);
-OneWire ds(liquid_temperature_pin); //2번 핀과 연결되 OneWire 객체 생성
+#define pH_Offset 0.00
+
+DHT11 dht11(humidity_temperature_pin);
+OneWire ds(liquid_temperature_pin);
+DallasTemperature sensors(&ds);
+GravityTDS gravityTds;
 
 #define PATH    "in"
 // ethernet interface mac address, must be unique on the LAN
 byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
-
 const char website[] PROGMEM = "ec2-13-125-23-94.ap-northeast-2.compute.amazonaws.com";
 
-String uuid = "57 39 39 31 34 31 10 15 0E";
+int pHArray[40];
+int pHArrayIndex=0;
+
+String uuid;
 float liquid_temperature;
 float temperature;
 float humidity;
 float liquid_flow_rate = 25.5;
-float ph = 7.1;
-float ec = 0.12;
+float pH=7;
+float ec;
 int light;
 boolean liquid_level;
 boolean valve = 0;
 boolean led = 0;
-boolean fan = 0;
+boolean fan;
 
-byte Ethernet::buffer[500];
+byte Ethernet::buffer[700];
 uint32_t timer;
 Stash stash;
+
+void read_uuid(){
+  for (size_t i = 0; i < 8; i++)
+  {
+    if (UniqueID8[i] < 0x10)
+      uuid += "0";
+    uuid += String(UniqueID8[i], HEX);
+  }
+}
+
+int read_light(){
+  return analogRead(light_pin);
+}
+
+void read_humidity_and_temperature(){
+  dht11.read(humidity, temperature);
+}
+
+boolean read_liquid_level(){
+  return digitalRead(liquid_level_pin);
+}
+
+float read_liquid_temperature(){
+  float tempC;
+  sensors.requestTemperatures();
+  tempC = sensors.getTempCByIndex(0);
+  tempC = round(tempC*10)/10.0;
+  return tempC;
+}
+
+float read_ec(){
+  float tdsValue;
+  gravityTds.setTemperature(liquid_temperature);  // set the temperature and execute temperature compensation
+  gravityTds.update();  //sample and calculate
+  tdsValue = gravityTds.getTdsValue();  // then get the value
+  tdsValue = round(tdsValue)/500;
+  tdsValue = round(tdsValue*10)/10.0;
+  return tdsValue;
+}
+
+
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }
+  else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
+float read_pH(){  
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > 20)
+  {
+    pHArray[pHArrayIndex++]=analogRead(pH_pin);
+    if(pHArrayIndex==40)pHArrayIndex=0;
+    voltage = avergearray(pHArray, 40)*5.0/1024;
+    pHValue = 3.5*voltage+pH_Offset;
+    samplingTime=millis();
+  }
+  return round(pHValue*10)/10.0;
+}
 
 void setup () {
   //시리얼 통신 셋팅
@@ -42,8 +152,8 @@ void setup () {
   pinMode(liquid_level_pin, INPUT);
 
   //네트워크 셋팅
-  Serial.println("\n[webClient]");
-  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+  Serial.println("\n[Smart Farm]");
+  if (ether.begin(sizeof Ethernet::buffer, mymac, 53) == 0) 
     Serial.println( "Failed to access Ethernet controller");
   if (!ether.dhcpSetup())
     Serial.println("DHCP failed");
@@ -53,149 +163,89 @@ void setup () {
   if (!ether.dnsLookup(website))
     Serial.println("DNS failed");
   ether.printIp("SRV: ", ether.hisip);
+
+  //ec sensor setting
+  gravityTds.setPin(ec_pin);
+  gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();  //initialization
+
+  pinMode(fan_pin, OUTPUT);
+  digitalWrite(fan_pin, HIGH);
+  
+  //read_uuid
+  read_uuid();
+
   
 }
 
 void loop () {  
   ether.packetLoop(ether.packetReceive());
+  //pH
+  pH = read_pH();
+
   if (millis() > timer) {
+    //1000 -> 1sec
     timer = millis() + 5000;
     Serial.println();
-    Serial.println("<<< REQ ");
-
-    liquid_temperature = round(read_liquid_temperature()*10)/10.0;
-    
-    light = read_light();
-    light = map(light, 0, 1023, 100, 0);
-    
-    dht11.read(humidity, temperature);
-    temperature = round(temperature*10)/10.0;
-    
-    liquid_level = read_liquid_level();
+    Serial.print("<<< REQ ");
     
     //포트번호
     ether.hisport = 5000;
+
+    //liquid_temperature
+    liquid_temperature = read_liquid_temperature();
+
+    //
+    read_humidity_and_temperature();
+    temperature = round(temperature*10)/10.0;
+    
+    //ec
+    ec = read_ec();
+    
+    //liquid_level
+    liquid_level = read_liquid_level();
+    
+    //light
+    light = read_light();
+    light = map(light, 0, 1023, 0, 100);
+    
     byte sd = stash.create();
-    String jsondata = "";
-    //StaticJsonBuffer<320> jb;
-    //DynamicJsonBuffer jb;
-    //JsonObject& obj = jb.createObject();
-
-    const size_t capacity = JSON_OBJECT_SIZE(12);
-
-    //DynamicJsonDocument obj(2048);
-    StaticJsonDocument<64> obj;
+    char jsondata[250];
+    const size_t capacity = JSON_OBJECT_SIZE(12)+50;
+    DynamicJsonDocument doc(capacity);
+    doc["uuid"] = uuid;
+    doc["liquid_temperature"] = liquid_temperature;
+    doc["temperature"] = temperature;
+    doc["humidity"] = humidity;
+    doc["liquid_flow_rate"] = liquid_flow_rate;
+    doc["pH"] = pH;
+    doc["ec"] = ec;
+    doc["light"] = light;
+    doc["liquid_level"] = liquid_level;
+    doc["valve"] = valve;
+    doc["led"] = led;
+    doc["fan"] = fan;
     
-    //obj["uuid"] = uuid;
-    //obj["liquid_temperature"] = liquid_temperature;
-    //obj["temperature"] = temperature;
-    //obj["humidity"] = humidity;
-    //obj["liquid_flow_rate"] = liquid_flow_rate;
-    //obj["ph"] = ph;
-    //obj["ec"] = ec;
-    //obj["light"] = light;
-    //obj["liquid_level"] = liquid_level;
-    //obj["valve"] = valve;
-    //obj["led"] = led;
-    //obj["fan"] = fan;
-    //serializeJson(obj, jsondata);
-    jsondata += "{\"uuid\":\"";
-    jsondata += uuid;
-    jsondata += "\",\"liquid_temperature\":";
-    jsondata += liquid_temperature;
-    jsondata += ",\"temperature\":";
-    jsondata += temperature;
-    jsondata += ",\"humidity\":";
-    jsondata += humidity;
-    jsondata += ",\"liquid_flow_rate\":";
-    jsondata += liquid_flow_rate;
-    jsondata += ",\"ph\":";
-    jsondata += ph;
-    jsondata += ",\"ec\":";
-    jsondata += ec;
-    jsondata += ",\"light\":";
-    jsondata += light;
-    jsondata += ",\"liquid_level\":";
-    jsondata += liquid_level;
-    jsondata += ",\"valve\":";
-    jsondata += valve;
-    jsondata += ",\"led\":";
-    jsondata += led;
-    jsondata += ",\"fan\":";
-    jsondata += fan;
-    
-    
-    jsondata += "}";
-    //\",\"liquid_temperature\":21,\"temperature\":29,\"humidity\":67,\"liquid_flow_rate\":25.51,\"ph\":7.1,\"ec\":0.12,\"light\":100,\"liquid_leve\":0,\"velve\":0, \"led\":0, \"fan\":0}"
-    //obj.printTo(jsondata);
-    
+    serializeJson(doc, jsondata, sizeof(jsondata));
+    Serial.print("sizeof(jsondata) : ");
+    Serial.println(sizeof(jsondata));
     Serial.println(jsondata);
     stash.print(jsondata);
-    //stash.print("light=");
-    //stash.print(light);
-    //stash.print("&humidity=");
-    //stash.print(humidity);
-    //stash.print("&temperature=");
-    //stash.print(temperature);
-    int stash_size = stash.size();
-    Serial.println(stash_size);
     stash.save();
     
     // generate the header with payload - note that the stash size is used,
     // and that a "stash descriptor" is passed in as argument using "$H"
-    Stash::prepare(PSTR("POST https://$F/$F HTTP/1.1" "\r\n"
+    //send post json
+    Stash::prepare(PSTR("POST http://$F/$F HTTP/1.1" "\r\n"
                 "Host: $F" "\r\n"
                 "Content-Length: $D" "\r\n"
                 "Content-Type: application/json" "\r\n"
                 "\r\n"
                 "$H"),
-    website, PSTR(PATH), website, stash_size, sd);
+    website, PSTR(PATH), website, stash.size(), sd);
     // send the packet - this also releases all stash buffers once done
     ether.tcpSend();
+
   }
-}
-
-int read_light(){
-  return analogRead(A0);
-}
-
-void read_Humidity_and_Temperature(){
-  dht11.read(humidity, temperature);
-}
-
-boolean read_liquid_level(){
-  return digitalRead(liquid_level_pin);
-}
-
-//온도 측정 후 반환하는 함수
-float read_liquid_temperature(){
- byte data[12];
- byte addr[8];
- if ( !ds.search(addr)) {
-   ds.reset_search();
-   return -3000;
- }
- if ( OneWire::crc8( addr, 7) != addr[7]) {
-   Serial.println("CRC is not valid!");
-   return -2000;
- }
- if ( addr[0] != 0x10 && addr[0] != 0x28) {
-   Serial.print("Device is not recognized");
-   return -1000;
- }
- ds.reset();
- ds.select(addr);
- ds.write(0x44,1);    //변환
- byte present = ds.reset();
- ds.select(addr);  
- ds.write(0xBE); 
- for (int i = 0; i < 9; i++) { 
-  data[i] = ds.read();  //Scratchpad 읽음
- }
- ds.reset_search();
- byte MSB = data[1];
- byte LSB = data[0];
- float tempRead = ((MSB << 8) | LSB); 
- float TemperatureSum = tempRead / 16;
- return TemperatureSum;
 }
